0e6eeaaa9aaf34d336caa981bf443f166a496409
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-06-16 01:10:46 +0200

    feat: add new layout component, declare new variable

--- app/src/main/java/de/uni_s/ipvs/mcl/assignment3/MainActivity.java
+++ app/src/main/java/de/uni_s/ipvs/mcl/assignment3/MainActivity.java
@@ -3,20 +3,74 @@ package de.uni_s.ipvs.mcl.assignment3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mLocationEditText;
+    private EditText mTemperatueEditText;
+    private Button mSendButton;
+
+
+    // Firebase instance variables
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMessagesDatabaseReference;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+
+        // Initialize Firebase components
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference("message");
+
+        // Initialize references to views
+        mSendButton = (Button) findViewById(R.id.sendButton);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mTemperatueEditText = (EditText) findViewById(R.id.temperatureEditText);
+
+        // Enable Send button when there's text to send
+        mLocationEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        // Send button sends a message and clears the EditText
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Send messages on click
+                // mMessagesDatabaseReference.push().setValue();
+
+                // Clear input box
+                mLocationEditText.setText("");
+            }
+        });
+
+        // TODO: read from database
     }
 }
